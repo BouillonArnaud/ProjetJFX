003a840f05890133604cbc8ca7b939fd9fd52fdc
“Alessio“ <la228805@student.helha.be>
2025-03-27 23:43:59 +0100

    Add popup on pawn move + JSON management

--- pProjetJFX2025/src/controllers/BoardController.java
+++ pProjetJFX2025/src/controllers/BoardController.java
@@ -1,8 +1,13 @@
 package controllers;
 
+import com.google.gson.JsonObject;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import model.GameBoard;
+import model.JsonUtils;
 import views.BoardView;
 
 public class BoardController {
@@ -16,10 +21,36 @@ public class BoardController {
 
     public void handleKeyPress(KeyEvent event) {
         if (event.getCode() == KeyCode.RIGHT) {
-            board.deplacerPion(1); // Avancer d'une case
+            if (board.deplacerPion(1)) {
+                showCasePopup("Pion déplacé à droite"); // APPEL ICI
+            }
         } else if (event.getCode() == KeyCode.LEFT) {
-            board.deplacerPion(-1); // Reculer d'une case
+            if (board.deplacerPion(-1)) {
+                showCasePopup("Pion déplacé à gauche"); // ET ICI
+            }
         }
         boardView.updatePionPosition();
+    }
+    
+    //read the Json file 
+    /*public void loadConfig() {
+        try {
+            JsonObject config = JsonUtils.readJson("src/resources/game_config.json");
+            
+            // Exemple : Extraire une valeur
+            String boardColor = config.get("boardColor").getAsString();
+            System.out.println("Couleur du plateau : " + boardColor);
+
+        } catch (Exception e) {
+            System.err.println("Erreur de lecture : " + e.getMessage());
+        }
+    }*/
+    
+    private void showCasePopup(String message) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Position du pion");
+        alert.setHeaderText("Nouvelle position : Case " + board.getPion().getIndex());
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
