117c43193dc553c89ba9908e2e762dc573e87baf
BouillonArnaud <la239012@student.helha.be>
2025-03-29 21:08:02 +0100

    "add doddis code to my branch"

--- pProjetJFX2025/src/views/BoardView.java
+++ pProjetJFX2025/src/views/BoardView.java
@@ -2,25 +2,32 @@ package views;
 
 import java.io.File;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.Case;
 import model.GameBoard;
 
 public class BoardView extends Pane {
     private final GameBoard board;
-    private final ImageView pionGraphique;
-    private final Color[] couleurs = {Color.PURPLE, Color.ORANGE, Color.BLUE, Color.GREEN,
+    private final ImageView pawnView;
+    private final Color[] colors = {Color.PURPLE, Color.ORANGE, Color.BLUE, Color.GREEN,
                                       Color.ORANGE, Color.ORANGE, Color.PURPLE, Color.GREEN};
     private static final int RECT_WIDTH = 146; // Largeur des rectangles
     private static final int RECT_HEIGHT = 90; // Hauteur des rectangles
@@ -40,7 +47,7 @@ public class BoardView extends Pane {
         int index = 0;
         for (Case c : board.getChemin()) {
             Rectangle casePlateau = new Rectangle(RECT_WIDTH, RECT_HEIGHT);
-            casePlateau.setFill(couleurs[index % couleurs.length]);
+            casePlateau.setFill(colors[index % colors.length]);
             casePlateau.setStroke(Color.BLACK);
             casePlateau.setX(c.getX() * RECT_WIDTH / 40); // Ajustement des proportions
             casePlateau.setY(c.getY() * RECT_HEIGHT / 40);
@@ -48,13 +55,13 @@ public class BoardView extends Pane {
             index++;
         }
 
-        // Dessiner le pion
-        Image pionImage = new Image(getClass().getResource("/resources/pawns1.png").toExternalForm()); // Mets le bon chemin
-        pionGraphique = new ImageView(pionImage);
-        pionGraphique.setFitWidth(RECT_HEIGHT / 2);  // Ajuste la taille du pion
-        pionGraphique.setFitHeight(RECT_HEIGHT / 2);
-        updatePionPosition();
-        this.getChildren().add(pionGraphique);
+     // Draw pawn
+        Image pawnImage = new Image(getClass().getResource("/resources/pawns1.png").toExternalForm());
+        pawnView = new ImageView(pawnImage);
+        pawnView.setFitWidth(RECT_HEIGHT / 2);
+        pawnView.setFitHeight(RECT_HEIGHT / 2);
+        updatePawnPosition();
+        this.getChildren().add(pawnView);
         
        StackPane btnMenu = createButton("Return to menu");
        
@@ -82,12 +89,70 @@ public class BoardView extends Pane {
         mediaPlayer = new MediaPlayer(sound);
     }
 
-    public void updatePionPosition() {
+    public void updatePawnPosition() {
         Case currentCase = board.getChemin().get(board.getPion().getIndex());
         
-        pionGraphique.setX(currentCase.getX() * RECT_WIDTH / 40 + RECT_WIDTH / 8);
-        pionGraphique.setY(currentCase.getY() * RECT_HEIGHT / 40 + RECT_HEIGHT / 2);
+        // Update pawn position
+        pawnView.setX(currentCase.getX() * RECT_WIDTH / 40 + RECT_WIDTH / 8);
+        pawnView.setY(currentCase.getY() * RECT_HEIGHT / 40 + RECT_HEIGHT / 2);
+        
+        // Show popup with case color
+        int caseIndex = board.getPion().getIndex();
+        showCasePopup(colors[caseIndex % colors.length]);
     }
+    
+    /**
+     * Displays a colored popup window with case information
+     * @param caseColor The background color for the popup
+     */
+    private void showCasePopup(Color caseColor) {
+        Stage popupStage = new Stage();
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        
+        VBox content = new VBox(20);
+        content.setAlignment(Pos.CENTER);
+        content.setPadding(new Insets(20));
+        content.setStyle("-fx-background-color: #" + caseColor.toString().substring(2, 8) + ";");
+        
+        // Title
+        Label title = new Label("Case Details");
+        title.setStyle("-fx-font-size: 20; -fx-text-fill: white; -fx-font-weight: bold;");
+        
+        // Information
+        int caseIndex = board.getPion().getIndex();
+        Label info = new Label("Case #" + caseIndex + "\nColor: " + getColorName(caseColor));
+        info.setStyle("-fx-font-size: 16; -fx-text-fill: white;");
+        info.setTextAlignment(TextAlignment.CENTER);
+        
+        // Close button
+        Button closeButton = new Button("Close");
+        closeButton.setOnAction(e -> popupStage.close());
+        
+        content.getChildren().addAll(title, info, closeButton);
+        
+        Scene scene = new Scene(content, 350, 250);
+        popupStage.setScene(scene);
+        popupStage.showAndWait();
+    }
+
+    /**
+     * Converts JavaFX Color to color name
+     * @param color The color to convert
+     * @return The color name in English
+     */
+    private String getColorName(Color color) {
+        if (color.equals(Color.PURPLE)) return "Purple";
+        if (color.equals(Color.ORANGE)) return "Orange";
+        if (color.equals(Color.BLUE)) return "Blue";
+        if (color.equals(Color.GREEN)) return "Green";
+        return "Unknown";
+    }
+
+    /**
+     * Creates a styled button
+     * @param textContent Button text
+     * @return Configured StackPane button
+     */
 
     public StackPane createButton(String textContent) {
         Rectangle rectangle = new Rectangle(100, 50);
